76584036683bf9bc8deebe2b8d29f2b0c1f90f46
Oliver Plit <oliverplit2005@gmail.com>
2025-11-03 16:53:15 +0100

    oliver hardcode menu and header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./main/header/header";
+import { Menu } from "./main/menu/menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Menu],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/main/menu/menu.ts
+++ src/app/main/menu/menu.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule, MatSidenavModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
 export class Menu {
+  showFiller = false;
 
 }
