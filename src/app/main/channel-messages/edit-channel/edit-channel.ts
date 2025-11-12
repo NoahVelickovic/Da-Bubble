a1a63395ebfa8f73346f08282e7a4f0dd4fdd21e
Oliver Plit <oliverplit2005@gmail.com>
2025-11-12 22:09:19 +0100

    edit and other dialogs

--- src/app/main/channel-messages/edit-channel/edit-channel.ts
+++ src/app/main/channel-messages/edit-channel/edit-channel.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-edit-channel',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './edit-channel.html',
   styleUrl: './edit-channel.scss',
 })
 export class EditChannel {
+  dialogRef = inject(MatDialogRef<EditChannel>);
+  showInputName = false;
+  showInputDescription = false;
+  closeName = true;
+  closeDescription = true;
+
+  close() {
+    this.dialogRef.close();
+  }
+
+
+  toggleEditName() {
+    this.showInputName = true;
+    this.closeName = false;
+  }
+
+  toggleEditDescription() {
+    this.showInputDescription = true;
+    this.closeDescription = false;
+
+  }
+
+   saveEditName() {
+    this.showInputName = false;
+    this.closeName = true;
+  }
+
+  saveEditDescription() {
+    this.showInputDescription = false;
+    this.closeDescription = true;
+
+  }
+
 
 }
--- src/app/main/menu/add-channel/add-people/add-people.ts
+++ src/app/main/menu/add-channel/add-people/add-people.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-people',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-people.html',
   styleUrl: './add-people.scss',
 })
 export class AddPeople {
+  dialogRef = inject(MatDialogRef<AddPeople>)
+  showExtraFields = false;
+
+  toggleExtraField(status: boolean) {
+    this.showExtraFields = status;
+  }
+
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
 
 }
